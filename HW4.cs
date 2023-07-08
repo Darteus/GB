c52f6c07899edeee5183c2307c752b012fd3f69e
Darteus <138676156+Darteus@users.noreply.github.com>
2023-07-08 19:17:16 +0300

    Add files via upload

---
+++ HW4.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int first = 2;
+if (num > 1)
+{
+    while (first <= num)
+    {
+        Console.Write(first + " ");
+        first += 2;
+    }
+}

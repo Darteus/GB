d3120c20c1908f4536d8d0743fa4188f5b648816
Darteus <138676156+Darteus@users.noreply.github.com>
2023-07-15 21:46:30 +0300

    Add files via upload

--- HW1_sem3.cs
+++ HW1_sem3.cs
@@ -1,39 +1,28 @@
-﻿// Задача 19 
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
-// 23432 -> да
+// 23432 -> да)
 
-using System.Linq;
-string num = Console.ReadLine();
-int [] num_n = num.Select(num => int.Parse(num.ToString())).ToArray();
-int [] arrT = new int[num_n.Length];
-int count = num_n.Length;
-int i = 0;
-    while (i < count)
-    { 
-        arrT[i] = int.Parse(arrT[i]+ "");
-        i++; 
-    }
-
-void Palin(string num_n)
+void DefPal(int num)
 {
-    for (int i = 0; i < 2; i++)
+    int first = num / 10000;
+    int second = num / 1000 % 10;
+    int fourth = num % 100 / 10;
+    int fifth = num % 10;
+    if (first == fifth && second == fourth)
     {
-        if (arrT[i] == arrT[count - 1]) i++; count--;
-        {
-            if (arrT[i] == arrT[count - 1])
-            {
-                Console.WriteLine(num_n + " -> да");
-                Console.WriteLine(arrT[i] + "");
-            }
-            else
-        {
-            Console.WriteLine("нет");
-        }
-        }
+        Console.WriteLine($"{num} -> yes");
     }
-    
+    else
+    {
+        Console.WriteLine($"{num} ->no");
+    }
 }
-
-Palin(num); 
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+int num = GetInfo("Введите число: ");
+DefPal(num);

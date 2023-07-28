ae41882a15f69ddc2055e07328092110c5b440b5
Darteus <138676156+Darteus@users.noreply.github.com>
2023-07-28 14:19:25 +0300

    Add files via upload
    Homework sem 6

---
+++ HW1_sem6.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int DefCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                count++;
+            }   
+        }
+        return count;
+}
+
+int GetInfo(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = GetInfo("Введите первое число ");
+int b = GetInfo("Введите второе число ");
+int c = GetInfo("Введите третье число ");
+int d = GetInfo("Введите четверое число ");
+int e = GetInfo("Введите пятое число ");
+int[] array = new int[] { a, b, c, d, e };
+
+Console.WriteLine(string.Join(", ", array) + " -> " + DefCount(array));
---
+++ HW2_sem6.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// k1 * x + b1 = k2 * x + b2; x * (k1 - k2) = b2 - b1; x = b2-b1/(k1-k2)
+
+double DefCoor(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return x;
+    return y;
+}
+
+int GetInfo(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double b1 = GetInfo("Введите координату х первой прямой");
+double k1 = GetInfo("Введите координату у первой прямой");
+double b2 = GetInfo("Введите координату х второй прямой");
+double k2 = GetInfo("Введите координату у второй прямой");
+double x = DefCoor(b1, k1, b2, k2);
+double y = DefCoor(b1, k1, b2, k2);
+
+Console.WriteLine("b1 = " + b1 + ", " + "k1 = " + k1 + ", " + "b2 = " + b2 + ", " + "k2 = " + k2 + " - > (" + string.Join("; ", x, y) +")");

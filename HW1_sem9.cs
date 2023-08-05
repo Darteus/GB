73ca5f8abf631a8c8b3e2d8ea10eec61962ef88d
Darteus <138676156+Darteus@users.noreply.github.com>
2023-08-05 19:00:59 +0300

    Add files via upload
    Seminar 9

---
+++ HW1_sem9.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string Rec(int lastNum, int firstNum)
+{
+    if (lastNum > firstNum) return $"{lastNum}" + ", " + Rec(lastNum-1, firstNum);
+    else return Convert.ToString(firstNum);
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInfo("start number = ");
+int n = 1;
+Console.WriteLine($"N = {m} - > {Rec(m, n)}");
---
+++ HW2_sem9.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void Rec(int firstNum, int lastNum)
+{
+    Console.Write($"M = {firstNum}; N = {lastNum} - > " + Sum(firstNum-1, lastNum));
+}
+int Sum(int firstNum, int lastNum)
+{
+    int sum = firstNum;
+    if (firstNum == lastNum) return 0;
+    else 
+        firstNum++;
+        sum = firstNum + Sum(firstNum, lastNum);
+        return sum;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInfo("start number = ");
+int n = GetInfo("end number = ");
+Rec(m, n);
